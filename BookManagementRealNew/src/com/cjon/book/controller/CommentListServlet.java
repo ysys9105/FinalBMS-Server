5eb2660162776971338aba9cdd100ae010ebf2e8
kang yoon sik <ysys9105@gmail.com>
2016-11-07 18:11:03 +0900

    완료

--- BookManagementRealNew/src/com/cjon/book/controller/CommentListServlet.java
+++ BookManagementRealNew/src/com/cjon/book/controller/CommentListServlet.java
@@ -18,7 +18,7 @@ import com.cjon.book.service.BookService;
 public class CommentListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
+    /** 
      * @see HttpServlet#HttpServlet()
      */
     public CommentListServlet() {
